990c8fc11880c2908184bbdc8b1b8694b482b905
swapnaliss <swapnalisbhargude123@gmail.com>
2023-08-13 12:43:00 +0530

    UI done for Movie listing page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
+import '../App.css';
 import { useNavigate } from 'react-router-dom';
 
+const MovieCard = ({ movie, showActions = true }) => {
 
-const MovieCard = ({ movie }) => {
     const navigate = useNavigate();
 
+  const toggleStarred = () => {
+    movie.isStarred = !movie.isStarred;
+  };
+
+  const toggleWatchlist = () => {
+    movie.isAddedToWatchlist = !movie.isAddedToWatchlist;
+  };
+
   return (
-    <Card style={{ width: '18rem' }} 
-    onClick={() => navigate(`/MovieDetailesPage/${movie.id}`)}>
-        
-      <Card.Img variant="top" src={movie.imageURL} />
+    <Card style={{ width: '18rem' }} className='cards'
+    onClick={() => navigate(`/MovieDetailesPage/${movie.id}`)}
+    >
+      <Card.Img variant="top" src={movie.imageURL} alt={movie.title} />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
         <Card.Text>{movie.summary}</Card.Text>
+        {showActions && (
+          <div>
+            <Button variant={movie.isStarred ? 'warning' : 'outline-warning'} onClick={toggleStarred}>
+              {movie.isStarred ? 'Starred' : 'Star'}
+            </Button>
+            <Button
+              variant={movie.isAddedToWatchlist ? 'success' : 'outline-success'}
+              className="ml-2"
+              onClick={toggleWatchlist}
+            >
+              {movie.isAddedToWatchlist ? 'Added to Watchlist' : 'Add to Watchlist'}
+            </Button>
+          </div>
+        )}
       </Card.Body>
-     
     </Card>
   );
 };
--- src/components/MovieListing.jsx
+++ src/components/MovieListing.jsx
@@ -1,19 +1,62 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col,Form , Button} from 'react-bootstrap';
 import { movies } from './MoviesData'; 
 import MovieCard from './MovieCard';
+import '../App.css';
 
 const MovieListing = () => {
   return (
+    <div>
+    <div className='Header'>
+     <h3>Movies</h3>
+     <Form.Group controlId="genreFilter">
+        <Form.Control as="select" >
+          <option value="All ">All Genre</option>
+          <option value="Drama">Drama</option>
+          <option value="Crime">Crime</option>
+          <option value="Action">Action</option>
+        </Form.Control>
+      </Form.Group>
+     
+      <Form.Group controlId="yearFilter">
+        <Form.Control as="select" >
+          <option value="All ">Release Year </option>
+          <option value="Drama">1994</option>
+          <option value="Crime">1992</option>
+          <option value="Action">1999</option>
+          <option value="Action">1991</option>
+          <option value="Action">2008</option>
+          <option value="Action">2010</option>
+          <option value="Action">2001</option>
+          <option value="Action">2003</option>
+
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group controlId="ratingsFilter">
+        <Form.Control as="select" >
+          <option value="All ">Rating </option>
+          <option value="Drama">8</option>
+          <option value="Crime">9</option>
+          <option value="Action">10</option>
+        </Form.Control>
+      </Form.Group>
+
+      <Button variant="primary" >
+        Add Movie
+      </Button>
+    </div>
+   
     <Container className='movieList'>
       <Row>
         {movies.map((movie) => (
           <Col key={movie.id} xs={12} sm={6} md={4} lg={3}>
-            <MovieCard movie={movie} />
+            <MovieCard movie={movie} showActions={true} />
           </Col>
         ))}
       </Row>
     </Container>
+    </div>
   );
 };
 
